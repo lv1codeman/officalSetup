ae6d12f73a09c3d07b6253bdb9e7b5501bfe98fc
lv1codeman <lv1codeman@gmail.com>
2024-04-11 22:46:02 +0800

    01

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text">
+
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: []
+    }
+  },
+  methods: {
+    getPosts() {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => this.post = data)
+        .then(data => console.log(data))
+
+      
+    }
+  }
+}
+</script>
